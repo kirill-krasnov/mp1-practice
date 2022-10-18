3d9168296dd105c547eeaa8bd3bf43ce277cd6e2
Kirill Krasnov <113035847+kirill-krasnov@users.noreply.github.com>
2022-10-18 18:09:20 +0300

    Update Source.c

--- KrasnovK.A Practice1/Source.c
+++ KrasnovK.A Practice1/Source.c
@@ -3,34 +3,29 @@
 
 int main() {
 
-	float h, w, d, m, dvp, dsp, wood, m1, m2, m3, m4, m5;
+	float h, w, d, m, dvp = 750, dsp = 800, wood = 650, m_back, m_sides, m_ud, m_doors, m_sh;
 
 	printf("Input height (180-220), width(80-120), depth (50-90) of the cupboard (cm)");
 	scanf_s("%f %f %f", &h, &w, &d);
 
 	if ((h < 180) || (h > 220) || (w < 80) || (w > 120) || (d < 50) || (d > 90)) {
-		printf("Uncorrect data, idiot");
+		printf("Uncorrect data");
 		return 0;
 	}
 
-	dvp = 750;
-	dsp = 450;
-	wood = 650;
-
-	m1 = ((h * w * 0, 5) / 1000000) * dvp;
-	m2 = (((h * d * 1, 5) / 1000000) * dsp) * 2;
-	m3 = (((w * d * 1, 5) / 1000000) * dsp) * 2;
-	m4 = ((h * w) / 1000000) * wood;
+	m_back = h * w * 0.5 * dvp / 1000000;
+	m_sides = h * d * 1.5 * dsp * 2 / 1000000;
+	m_ud = (w - 3) * (d - 0.5) * 1.5 * dsp * 2 / 1000000;
+	m_doors = h * w * wood / 1000000;
 	if (h > 200) {
-		m5 = (((((w - 3) * (d - 0, 5) * 1, 5) / 1000000) * dsp) * 5);
+		m_sh = (w - 3) * (d - 0.5) * 1.5 * dsp * 5 / 1000000;
 	}
 	else {
-		m5 = (((((w - 3) * (d - 0, 5) * 1, 5) / 1000000) * dsp) * 4);
+		m_sh = (w - 3) * (d - 0.5) * 1.5 * dsp * 4 / 1000000;
 	}
-	m = m1 + m2 + m3 + m4 + m5;
+	m = m_back + m_sides + m_ud + m_doors + m_sh;
 
 	printf("m = %f", m);
 
 	return 0;
-
 }
